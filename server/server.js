93092bc0c38271df07826d1b83568bfd56ff5321
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-01-24 16:59:47 -0800

    added test to messages, abstracted message generation

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require('express');
 const socketIO = require('socket.io');
 const PORT = process.env.PORT || 3000;
-
+const {generateMessage} = require('./utils/message')
 const app = express();
 const publicPath = path.join(__dirname, '../public');
 
@@ -12,26 +12,14 @@ const io = socketIO(server)
 
 io.on('connection', (socket)=>{
     console.log('New user connected')
-    socket.emit('welcome', {
-        from: 'Admin',
-        text: 'Welcome to Mother Russia Chat App',
-        createdAt: new Date().getTime()
-    })
-    socket.broadcast.emit('welcome', {
-        from: 'Admin',
-        text: 'New user has joined the channel',
-        createdAt: new Date().getTime()
-    })
+    socket.emit('welcome', generateMessage('Admin', 'Welcome to Mother Russia Chat App'))
+    socket.broadcast.emit('welcome', generateMessage('Admin', 'New user joined the chanel'))
     socket.on('disconnect', ()=>{
         console.log('User disconnected')
     });
     socket.on('createMessage', function(message){
         console.log('Recieved message', message)
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        io.emit('newMessage', generateMessage(message.from, message.text))
 
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
