3b723e1698fac2ed797a3df11c0e732036d3c8b1
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-01-25 15:04:04 -0800

    heroku config

--- server/utils/message.js
+++ server/utils/message.js
@@ -4,14 +4,14 @@ let generateMessage = (from, text) =>{
     return {
         from,
         text,
-        createdAt: moment().fromNow()
+        createdAt: moment().format("h:mm:ss a")
     }
 }
 let generateLocationMessage = (from, latitude, longitude) =>{
     return{
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt: moment().fromNow()
+        createdAt: moment().format("h:mm:ss a")
     }
 }
 
