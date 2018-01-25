bdaa2196fc3f1e5753a63187938de089337369e2
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-01-25 15:07:31 -0800

    moment config

--- server/utils/message.js
+++ server/utils/message.js
@@ -4,14 +4,14 @@ let generateMessage = (from, text) =>{
     return {
         from,
         text,
-        createdAt: moment().format("h:mm:ss a")
+        createdAt: moment().utcOffset("-8:00").format("h:mm:ss a")
     }
 }
 let generateLocationMessage = (from, latitude, longitude) =>{
     return{
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt: moment().format("h:mm:ss a")
+        createdAt: moment().utcOffset("-8:00").format("h:mm:ss A")
     }
 }
 
