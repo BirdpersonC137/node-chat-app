b8c943c07f6ad47fcc06ff58b1fc0bfdf06fdf67
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-01-31 16:15:28 -0800

    room name fix

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,20 @@ let roomlist;
 let options = [];
 socket.on('roomList', (rooms)=>{
     roomlist = rooms.roomList;
-    console.log(roomlist);
+        function capitalize(str) {
+        const words = []
+    
+        for(let word of str.split(' ')){
+            words.push(word[0].toUpperCase() + word.slice(1));
+        }
+        return words.join(' ')
+    } 
     $(document).ready(function(){
         let select = document.getElementById('rooms')
             roomlist.forEach((room)=>{
                 let option = document.createElement('option');
-                option.value = room;
-                option.innerHTML = room;
+                option.value = capitalize(room);
+                option.innerHTML = capitalize(room);
                 option.setAttribute('id', `${option.innerHTML}`)
                 optionId = option.getAttribute('id')
                 if(options.indexOf(optionId)===-1){
